e0d301a04d8cda38ab4cd1a293599be9c50de9da
Jesse Vang <jessevang@sbcglobal.net>
2025-02-13 17:41:25 -0800

    Added option to turn on community center event on day 1

--- MineOnDay1.cs
+++ MineOnDay1.cs
@@ -1,31 +1,26 @@
-﻿using StardewModdingAPI.Events;
+﻿
 using StardewModdingAPI;
 using StardewValley;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json;
-using System.Text.Json;
+
+
+
 
 namespace MineOnDay1
 {
+    //Create a class for our config file to define the properties
+    public class Config
+    {
+        public bool TurnOnMiningOnDay1 { get; set; } = true;
+        public bool TurnOnCommunityCenterOnDay1 { get; set; } = true;
 
-    /// <summary>The mod entry point.</summary>
+
+    }
+
     internal sealed class ModEntry : Mod
     {
         public uint realCurrentDay;
-        public Boolean TurnOn_EarlyingMining = false;
-        public const string ConfigFileName = "config.json";
-
-        private class Config
-        {
-            public int TurnOnEarlyMining { get; set; } = 1;
-
-        }
+        public static ModEntry Instance { get; private set; }
+        public Config Config { get; private set; }
 
         /*********
        ** Public methods
@@ -34,108 +29,89 @@ namespace MineOnDay1
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            var config = helper.ReadConfig<Config>() ?? new Config();
+            Instance = this; // Assign the instance for global access
+            Config = helper.ReadConfig<Config>() ?? new Config();
 
-            helper.Events.GameLoop.GameLaunched += onGameLaunch; //loads config file on game launch
             helper.Events.GameLoop.DayStarted += onStartOfDay;
             helper.Events.GameLoop.DayEnding += onEndOfDay;
 
-
-
-        }
-
-
-
-
-        //reads config file and sets and turns on certain mod features
-        private void onGameLaunch(object sender, GameLaunchedEventArgs e)
-        {
-            //read Config File
-            string filePath = Directory.GetCurrentDirectory() + "\\Mods\\MineOnDay1\\config.json";
-
-            try
+            if (Instance.Config.TurnOnCommunityCenterOnDay1)
             {
-
-                // Read the JSON file
-                string jsonString = File.ReadAllText(filePath);
-
-                // Deserialize the JSON string into a dynamic object
-                dynamic jsonObj = JsonConvert.DeserializeObject(jsonString);
-
-                // Access the properties of the JSON object
-                foreach (var item in jsonObj)
+                if (!helper.ModRegistry.IsLoaded("Pathoschild.ContentPatcher"))
                 {
-                    //Console.WriteLine($"{((JProperty)item).Name}: {((JProperty)item).Value}");
-
-                    string propertyName = ((JProperty)item).Name.ToString();
-
-                    if (propertyName == "TurnOnEarlyMining")
-                    {
-                        if ((uint)((JProperty)item).Value == 1)
-                        {
-                            TurnOn_EarlyingMining = true;
-                            //Console.WriteLine("TurnOnEarlyMining has been set to :" + true);
-                        }
-                        else if ((uint)((JProperty)item).Value == 0)
-                        {
-                            TurnOn_EarlyingMining = false;
-                            //Console.WriteLine("TurnOnEarlyMining has been set to :" + false);
-                        }
-
-
-                    }
-
-
+                    Monitor.Log("Content Patcher is not installed. Skipping patches.", LogLevel.Warn);
                 }
-            }
-
-            catch (FileNotFoundException)
-            {
-                Console.WriteLine($"File '{filePath}' not found.");
-            }
-            catch (Newtonsoft.Json.JsonException)
-            {
-                Console.WriteLine($"Invalid JSON format in '{filePath}'.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred: {ex.Message}");
-            }
+                else
+                {
+                    RegisterContentPatcherPack();
+                }
 
 
+            }
+           
 
         }
+
+        private void RegisterContentPatcherPack()
+        {
+            string contentPackPath = Path.Combine(Helper.DirectoryPath, "assets");
+
+            // Ensure content.json exists
+            if (!File.Exists(Path.Combine(contentPackPath, "Content.json")))
+            {
+                Monitor.Log("ERROR: content.json not found! Content Patcher pack will not be loaded.", LogLevel.Error);
+                return;
+            }
+
+            // Register the content pack as a temporary Content Patcher pack
+            var contentPack = Helper.ContentPacks.CreateTemporary(
+                directoryPath: contentPackPath,
+                id: "Darkmushu.MineOnDay1",
+                name: "MineOnDay1",
+                description: "Temporary Content Patcher pack for MineOnDay1",
+                author: "Darkmushu",
+                version: new SemanticVersion(1, 0, 0) // SMAPI's version format
+            );
+
+            if (contentPack != null)
+            {
+                Monitor.Log("Content Patcher pack registered successfully!", LogLevel.Info);
+            }
+            else
+            {
+                Monitor.Log("Failed to register Content Patcher pack!", LogLevel.Error);
+            }
+        }
+
+
 
         //Code to change day 1,2,3,4 to day 5 to open up the mines.
         private void onStartOfDay(object sender, EventArgs e)
         {
-            if (!TurnOn_EarlyingMining)
-                return;
-
-            //Console.WriteLine("CurrentDaysPlayed : " + Game1.stats.DaysPlayed);
-            this.realCurrentDay = (uint)Game1.stats.DaysPlayed;
-            // Check the number of days played
-            if (Game1.stats.DaysPlayed <= 5)
+            if (!Instance.Config.TurnOnMiningOnDay1)
             {
-                Game1.stats.DaysPlayed = 5; // Set the days played to 5
-                                            //Console.WriteLine("Days played set :" + Game1.stats.DaysPlayed);
+                this.realCurrentDay = (uint)Game1.stats.DaysPlayed;
+                // Check the number of days played
+                if (Game1.stats.DaysPlayed <= 5)
+                {
+                    Game1.stats.DaysPlayed = 5; // Set the days played to 5
+                }
             }
+
+
 
         }
 
         //resets the day back to the original current day at the end of the day so calendar
         private void onEndOfDay(object sender, EventArgs e)
         {
-            if (!TurnOn_EarlyingMining)
-                return;
-            // Console.WriteLine("CurrentDaysPlayed : " + Game1.stats.DaysPlayed);
-            Game1.stats.DaysPlayed = this.realCurrentDay;
-            //Console.WriteLine("Current Day has been set to : " + Game1.stats.DaysPlayed);
+            if (!Instance.Config.TurnOnMiningOnDay1)
+            {
+                Game1.stats.DaysPlayed = this.realCurrentDay;
+            }
+
 
         }
-
-
-
 
 
 
